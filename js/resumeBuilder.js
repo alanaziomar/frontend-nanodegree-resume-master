a1dad794847ec31db81924b6999c391100f991a2
omar alanazi <alanazi.omar@gmail.com>
2017-03-29 10:34:30 +0300

    new

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,133 +2,176 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- 
- 
  var bio ={
-	name:"omar alanazi",
-	role:"web developer",
-	contacts :["0502977008","alanazi.omar@gmail.com","alanaziomar","Riyadh, Saudi Arabia"],
-	welcomeMessage:"Welcome to my Resume",
-	skills:["Good communication","Self-management"],
-	biopic:"images/197x148.gif",
-	display:function(){
+ 	"name":"Omar alanazi",
+ 	"role":"Web developer",
+ 	"contacts":{
+ 		"mobile":"0502977008",
+ 		"email":"alanazi.omar@gmail.com",
+ 		"github":"alanaziomar",
+ 		"location":"Riyadh, Saudi Arabia"
+
+ 	},
+ 	"welcomeMessage":"Welcome to my Resume",
+ 	"skills":["Good communication","Self-management","Self-learnning"],
+ 	"biopic":"images/197x148.gif",
+ 	"display":function(){
+ 		var formattedName = HTMLheaderName.replace("%data%", bio["name"]);
+ 		var formattedRole = HTMLheaderRole.replace("%data%",bio["role"]);
+		$("#header").prepend(formattedRole);
+		$("#header").prepend(formattedName);
+
+			var formattedMobile = HTMLmobile.replace("%data%",bio.contacts.mobile);
+			var formattedemail = HTMLemail.replace("%data%",bio.contacts.email);
+			var formattedgithub = HTMLgithub.replace("%data%",bio.contacts.github);
+			var formattedliving = HTMLlocation.replace("%data%",bio.contacts.location);
+
+			$("#topContacts").append(formattedMobile);
+			$("#topContacts").append(formattedemail);
+			$("#topContacts").append(formattedgithub);
+			$("#topContacts").append(formattedliving);
+
+
+			$("#footerContacts").append(formattedMobile);
+			$("#footerContacts").append(formattedemail);
+			$("#footerContacts").append(formattedgithub);
+			$("#footerContacts").append(formattedliving);
 		
-		var role = HTMLheaderRole.replace("%data%", bio.role);
-		$("#header").prepend(role)
-		var headerName = HTMLheaderName.replace("%data%", bio.name);
-		$("#header").prepend(headerName);
-
-
-		var mob = HTMLmobile.replace("%data%", bio.contacts[0]);
-		$("#topContacts").append(mob);
-		var ema = HTMLemail.replace("%data%", bio.contacts[1]);
-		$("#topContacts").append(ema);
-		var git = HTMLgithub.replace("%data%", bio.contacts[2]);
-		$("#topContacts").append(git);
-		var loc = HTMLlocation.replace("%data%", bio.contacts[3]);
-		$("#topContacts").append(loc);
-
-		var mob = HTMLmobile.replace("%data%", bio.contacts[0]);
-		$("#footerContacts").append(mob);
-		var ema = HTMLemail.replace("%data%", bio.contacts[1]);
-		$("#footerContacts").append(ema);
-		var git = HTMLgithub.replace("%data%", bio.contacts[2]);
-		$("#footerContacts").append(git);
-		var loc = HTMLlocation.replace("%data%", bio.contacts[3]);
-		$("#footerContacts").append(loc);
 		
-		var Welcome =  HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-		$("#").prepend(Welcome);
+		var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%",bio["welcomeMessage"]);
+		$("#header").append(formattedWelcomeMessage);
+		var formattedBioPic = HTMLbioPic.replace("%data%",bio["biopic"]);
+		$("#header").append(formattedBioPic);
+		
+		if(bio.skills.length > 0){
+			$("#header").append(HTMLskillsStart);
+		for(skill in bio.skills){
+			
+			var formattedskill = HTMLskills.replace("%data%",bio.skills[skill]);
+			$("#skills").append(formattedskill);
+			}
+		}
+ 	}
+ };
+ 
+ var education ={
+ 	"schools":[{
+ 		"name":"Institute of Public Administration",
+ 		"location":"Riyadh, Saudi Arabia",
+ 		"degree":"Diploma",
+ 		"majors":["Computer Programming"],
+ 		"dates":"2011",
+ 		"url":"https://www.ipa.edu.sa"
+ 	}],
+ 	"onlineCourses":[{
+ 		"title":"Front End",
+ 		"school":"Udacity",
+ 		"dates":"2017",
+ 		"url":"https://www.Udacity.com"
+ 	}],
+ 	"display":function(){
+
+ 		education.schools.forEach(function(schools){
+
+			var formattedName = HTMLschoolName.replace("%data%",schools.name);
+			var formattedDegree = HTMLschoolDegree.replace("%data%",schools.degree);
+			var formattedNameDegrre = formattedName + formattedDegree;
+			var formattedLocation = HTMLschoolLocation.replace("%data%",schools.location);
+			var formattedMajors = HTMLschoolMajor.replace("%data%",schools.majors);
+			var formattedDates = HTMLschoolDates.replace("%data%",schools.dates);
 	
-		
-		var skills1 = HTMLskills.replace("%data%", bio.skills[0]);
-		$("#skills-h3").append(skills1);
-		var skills2 = HTMLskills.replace("%data%", bio.skills[1]);
-		$("#skills-h3").append(skills2);
+			$("#education").append(HTMLschoolStart);
+			$(".education-entry:last").append(formattedNameDegrre);
+			$(".education-entry:last").append(formattedLocation);
+			$(".education-entry:last").append(formattedMajors);
+			$(".education-entry:last").append(formattedDates);
+		});
 
-			var biopi =  HTMLbioPic.replace("%data%", bio.biopic);
-		$("#skills-h3").prepend(biopi);
+		education.onlineCourses.forEach(function(onlineCourses){
 
+			var formattedOnlineTitel = HTMLonlineTitle.replace("%data%",onlineCourses.title);
+			var formattedOnlineSchool = HTMLonlineSchool.replace("%data%",onlineCourses.school);
+			var formattedOnlineTitleSchool = formattedOnlineTitel + formattedOnlineSchool;
+			var formattedOnlineDates = HTMLonlineDates.replace("%data%",onlineCourses.dates);
+			var formattedOnlineUrl = HTMLonlineURL.replace("%data%",onlineCourses.url);
+	
+			$(".education-entry:last").append(HTMLonlineClasses);
+			$(".education-entry:last").append(formattedOnlineTitleSchool);
+			$(".education-entry:last").append(formattedOnlineDates);
+			$(".education-entry:last").append(formattedOnlineUrl);
+			
+		});
 
-	}
-};
-bio.display();
+ 	}
+ };
+ 
 var work ={
-	jobs:["kafa'at","developer","Riyadh, Saudi Arabia","april 2016","Test own developed web services (WS) to ensure WS reliability"],
-	display:function(){
-		
+	"jobs":[{
+		"employer":"kafa'at",
+		"title":"backend developer",
+		"location":"Riyadh, Saudi Arabia",
+		"dates":"Apr 2016",
+		"description":"develop back end using java"
+	},{
+		"employer":"elm",
+		"title":".Net developer",
+		"location":"Riyadh, Saudi Arabia",
+		"dates":"Apr 2013",
+		"description":"working with asp.net using C#"
+	}],
+	"display":function(){
+		for(job in work.jobs){
+			$("#workExperience").append(HTMLworkStart);
 
-		var emp = HTMLworkStart.replace("%data%", work.jobs);
-		$("#workExperience").append(emp);
-		var empk = HTMLworkEmployer.replace("%data%", work.jobs[0]);
-		$("#workExperience").append(empk);
-		var empt = HTMLworkTitle.replace("%data%", work.jobs[1]);
-		$("#workExperience").append(empt);
-		var empd = HTMLworkDates.replace("%data%", work.jobs[3]);
-		$("#workExperience").append(empd);
-		var emploc = HTMLworkLocation.replace("%data%", work.jobs[2]);
-		$("#workExperience").append(emploc);
-		var empdes = HTMLworkDescription.replace("%data%", work.jobs[4]);
-		$("#workExperience").append(empdes);
+			var formattedEmployee = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
+			var formattedTitel = HTMLworkTitle.replace("%data%",work.jobs[job].title);
+			var formattedEmployeeTitel = formattedEmployee + formattedTitel;
+			var formattedlocation = HTMLworkLocation.replace("%data%",work.jobs[job].location);
+			var formatteddates = HTMLworkDates.replace("%data%",work.jobs[job].dates);
+			var formatteddescription = HTMLworkDescription.replace("%data%",work.jobs[job].description);
 
-		
 
+
+			$(".work-entry:last").append(formattedEmployeeTitel);
+			$(".work-entry:last").append(formattedlocation);
+			$(".work-entry:last").append(formatteddates);
+			$(".work-entry:last").append(formatteddescription);
+		}
 	}
 };
-work.display();
 
-var education ={
-	schools:["Institute of Public Administration","Riyadh, Saudi Arabia","Diploma","Computer Programming","2011","https://www.ipa.edu.sa"],
-	onlineCourses:["Front End","Udacity","2017","htts://www.Udacity.com"],
-	display:function(){
+var projects ={
+	"projects":[{
+		"title":"Resume",
+		"dates":"2017",
+		"description":"build my resume using HTML, CSS ,and Javascript",
+		"images":["images/197x148.gif","images/197x148.gif"]
+	}],
+	"display":function(){
 
-		var sch = HTMLschoolStart.replace("%data%", education.schools);
-		$("#education").append(sch);
+			projects.projects.forEach(function(projects){
+			$("#projects").append(HTMLprojectStart);
 
-		var schn = HTMLschoolName.replace("%data%", education.schools[0]);
-		$("#education").append(schn);
-		var schd = HTMLschoolDegree.replace("%data%", education.schools[2]);
-		$("#education").append(schd);
-		var schdate = HTMLschoolDates.replace("%data%", education.schools[4]);
-		$("#education").append(schdate);
-		var schloc = HTMLschoolLocation.replace("%data%", education.schools[1]);
-		$("#education").append(schloc);
-		var schmaj = HTMLschoolMajor.replace("%data%", education.schools[3]);
-		$("#education").append(schmaj);
-		
-		var onlc = HTMLonlineClasses.replace("%data%", education.onlineCourses);
-		$("#education").append(onlc);
+			var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.title);
+			var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.dates);
+			var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.description);
+			var formattedProjectImage0 = HTMLprojectImage.replace("%data%", projects.images[0]);
+			var formattedProjectImage1 = HTMLprojectImage.replace("%data%", projects.images[1]);
 
-		var onlt = HTMLonlineTitle.replace("%data%", education.onlineCourses[0]);
-		$("#education").append(onlt);
-		var onls = HTMLonlineSchool.replace("%data%", education.onlineCourses[1]);
-		$("#education").append(onls);
-		var onld = HTMLonlineDates.replace("%data%", education.onlineCourses[2]);
-		$("#education").append(onld);
-		var onlu = HTMLonlineURL.replace("%data%", education.onlineCourses[3]);
-		$("#education").append(onlu);
-		
-
-
+			$(".project-entry:last").append(formattedProjectTitle);
+			$(".project-entry:last").append(formattedProjectDates);
+			$(".project-entry:last").append(formattedProjectDescription);
+			$(".project-entry:last").append(formattedProjectImage0);
+	 		$(".project-entry:last").append(formattedProjectImage1);
+		}) ;
 	}
 };
+
+$("#mapDiv").append(googleMap);
+
+
+
+bio.display();
 education.display();
-
-var projects={
-	proj:["Resume","2017","bulid my resume","images/fry.jpg"],
-	display:function(){
-		
-		var port = HTMLprojectStart.replace("%data%", projects.proj);
-		$("#projects").append(port);
-		var pord = HTMLprojectDates.replace("%data%", projects.proj[1]);
-		$("#projects").append(pord);
-		var pordes = HTMLprojectDescription.replace("%data%", projects.proj[2]);
-		$("#projects").append(pordes);
-
-	}
-};
 projects.display();
-
-
-var map = googleMap.replace( "<div id=”mapDiv”>");
-	$("#mapDiv").append(map);
+work.display();
